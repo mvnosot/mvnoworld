b855d308748594cb3672600abddf0e9a01167c81
zzihi <chorocta@naver.com>
2016-07-08 02:48:51 +0000

    test

--- routes/evnt_num/evntGoldNum.js
+++ routes/evnt_num/evntGoldNum.js
@@ -27,15 +27,18 @@ module.exports = function(app, User) {
     });
 */
 
-    /* GET home page. */
+    /* 골드번호 응모 화면 */
     app.get('/evnt_num', function(req, res) {
       res.render('evnt_num/evntGoldNum', { title: 'MVNO ROOM' });
     });
     
-    /* GET home page. */
-    app.get('/evnt_num', function(req, res) {
-      res.render('evnt_num/evntGoldNum', { title: 'MVNO ROOM' });
+    // 골드번호 응모
+    app.post('/evnt_num', function(req, res) {
+       res.end(); 
     });
-
-
+    
+    // 골드번호 응모결과 조회
+    app.get('/evnt_num/:svc_num', function(req, res) {
+       res.end(); 
+    });
 }
