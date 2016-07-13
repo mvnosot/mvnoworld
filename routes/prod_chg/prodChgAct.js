8e1dcab7d6e96f1e29b178c354eedad1473a5a13
Kim Yeonah <pyangru@naver.com>
2016-07-13 10:22:29 +0000

    20160713 ye0nah

--- routes/prod_chg/prodChgAct.js
+++ routes/prod_chg/prodChgAct.js
@@ -32,8 +32,9 @@ module.exports = function(app, ProdModel) {
     /* GET home page. */
 
     app.get('/prod_chg/:id', function(req,res){
+        console.log("prod_id:",req.params.id);
     ProdModel.findById(req.params.id, function(err,prod){
-        console.log("Data List:",prod);
+        console.log("Prod_info:",prod);
         if(err) return res.json({success:false,message:err});
         res.render("prod_chg/prodChgAction",{data:prod});
     });
