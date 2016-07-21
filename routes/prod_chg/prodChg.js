256c798c52b19b7cf2d95da07184fe8ff2ad8efb
Kim Yeonah <pyangru@naver.com>
2016-07-21 07:37:24 +0000

    20160721_kya

--- routes/prod_chg/prodChg.js
+++ routes/prod_chg/prodChg.js
@@ -1,10 +1,10 @@
 
 module.exports = function(app, CustModel, ProdModel) {
 
-
- app.get('/prod_chg', function(req, res) {
+    //요금제 조회
+    app.get('/prod_chg', function(req, res) {
         var custInfo = null;
-        CustModel.findOne({svc_num:"01029644930"},function(err,cust){
+        CustModel.findOne({svc_num:req.session.svc_num},function(err,cust){
          //console.log("cust :",cust);
           if(!err) custInfo = cust;
         });
@@ -15,11 +15,11 @@ module.exports = function(app, CustModel, ProdModel) {
         });
          
     });
-    
+    // 요금제 상세조회
     app.get('/prod_chg/:id', function(req,res){
         //console.log("prod_id:",req.params.id);
         var custInfo = null;
-        CustModel.findOne({svc_num:"01029644930"},function(err,cust){
+        CustModel.findOne({svc_num:req.session.svc_num},function(err,cust){
          //console.log("cust :",cust);
           if(!err) custInfo = cust;
         });
@@ -29,5 +29,20 @@ module.exports = function(app, CustModel, ProdModel) {
         res.render("prod_chg/prodChgAction",{data:prod, data2:custInfo});
         });
     }); 
+    //요금제 변경
+     app.post('/prod_chg', function(req, res) {
+        var prodInfo = req.body.prod;
+        var custInfo = null;
+        CustModel.findOne({svc_num:req.session.svc_num},function(err,cust){
+         //console.log("cust :",cust);
+          if(!err) custInfo = cust;
+        });
+        custInfo.fee_prod_id = prodInfo.prod_cd;
+        custInfo.fee_prod_nm = prodInfo.prod_nm;
+        CustModel.findByIdAndUpdate(custInfo._id, custInfo, function(err,cust){
+            if(err)return console.log("Update ERROR:",err);
+            res.redirect('/prod_chg');
+        });
+    });
     
 }
